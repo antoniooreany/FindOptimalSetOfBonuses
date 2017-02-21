ad4b11afdfce4288545638cdc164c2893e3ebf8c
Anton Gorshkov <antonio_oreany@mail.ru>
2017-02-21 15:15:34 +0300

    App generate all permutes of array and calculate when bonus became negative. Now I need to get cut arrays and find in this list of arrays the less negative bonuses.

--- src/Main.java
+++ src/Main.java
@@ -7,24 +7,21 @@ import java.util.List;
 public class Main {
 
     private static final int MAX_BONUS = 4;
-    private static final List<Integer> list = Arrays.asList(1, 2, 3);
-
+    private static final List<Integer> LIST_INTEGERS = Arrays.asList(1, 2, 3);
+    private static int listIndex;
+    private static int elemIndex;
 
     public static void main(String[] args) {
 
-        List<List<Integer>> lists = Permute.permute(list);
-//        System.out.println(lists);
-        for (List<Integer> permuteList : lists) {
+        List<Integer[]> lists = Permute.permute(LIST_INTEGERS);
+        for (Integer[] permuteList : lists) {
             int currentBonus = MAX_BONUS;
-            for (int elem : permuteList) {
+            for (Integer elem : permuteList) {
                 currentBonus = currentBonus - elem;
-                if (currentBonus <= 0) {
-                    System.out.println(permuteList.toString() + "    " + currentBonus);
+                if (currentBonus < 0) {
+                    System.out.println(/*permuteList.toString() + */Arrays.deepToString(permuteList) + "    " + currentBonus);
                 }
             }
-//            System.out.println(permuteList.toString() + "    " + currentBonus);
         }
-
     }
-
 }
--- src/Permute.java
+++ src/Permute.java
@@ -8,25 +8,31 @@ import java.util.List;
  */
 public class Permute {
 
-    private static List<List<Integer>> lists = new ArrayList<>();
+    private static List<Integer[]> lists = new ArrayList<>();
 
-    static List<List<Integer>> permute(List<Integer> arr, int k) {
+    static List<Integer[]> permute(List<Integer> arr, int k) {
         for (int i = k; i < arr.size(); i++) {
             Collections.swap(arr, i, k);
             permute(arr, k + 1);
             Collections.swap(arr, k, i);
         }
         if (k == arr.size() - 1) {
-            lists.add(arr);
+//            System.out.println(Arrays.toString(arr.toArray()));
+
+            Integer[] integers = (Integer[]) arr.toArray();
+            lists.add(integers);
         }
         return lists;
     }
 
-    static List<List<Integer>> permute(List<Integer> arr) {
+    static List<Integer[]> permute(List<Integer> arr) {
         return permute(arr, 0);
     }
 
     public static void main(String[] args) {
-        System.out.println(permute(Arrays.asList(1, 2, 3)));
+        List<Integer[]> listIntegers = permute(Arrays.asList(1, 2, 3));
+        for (Integer[] integers : listIntegers) {
+            System.out.println(Arrays.deepToString(integers));
+        }
     }
 }
